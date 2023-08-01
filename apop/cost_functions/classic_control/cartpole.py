6c150a07a4ed99f3697b3135d4e20627d5e93b79
Shunichi09 <quick1st97@gmail.com>
2023-08-02 00:45:18 +0900

    Add cartpole swing up cost function

---
+++ apop/cost_functions/classic_control/cartpole.py
@@ -0,0 +1,60 @@
+from typing import Optional
+from apop.cost_function import CostFunction
+
+import jax
+from jax import numpy as jnp
+from functools import partial
+
+import numpy as np
+
+
+class CartPoleCostFunction(CostFunction):
+    def __init__(
+        self, R: jnp.ndarray = jnp.eye(1) * 0.05, terminal_weight: float = 1.0
+    ):
+        super().__init__()
+        self._R = R
+        self._terminal_weight = terminal_weight
+
+    @partial(jax.jit, static_argnums=(0,))
+    def evaluate_stage_cost(
+        self, x: jnp.ndarray, u: Optional[jnp.ndarray], t: jnp.ndarray
+    ):
+        """Evaluate statge cost
+
+        Args:
+            x (jnp.ndarray): states, shape (state_size, )
+            u (jnp.ndarray): inputs, shape (input_size, )
+            t (jnp.ndarray): time step, shape (1, )
+
+        Returns:
+            jnp:ndarray: stage cost, shape (1, )
+        """
+        state_cost = (
+            6.0 * (x[0] ** 2)
+            + 12.0 * ((jnp.cos(x[2] - np.pi) + 1.0) ** 2)
+            + 0.1 * (x[1] ** 2)
+            + 0.1 * (x[3] ** 2)
+        )
+        input_cost = jnp.matmul(
+            u[jnp.newaxis, :], jnp.matmul(self._R, u[:, jnp.newaxis])
+        )
+        return (state_cost + input_cost).ravel()
+
+    @partial(jax.jit, static_argnums=(0,))
+    def evaluate_terminal_cost(self, x: jnp.ndarray, t: jnp.ndarray):
+        """Evaluate terminal cost
+
+        Args:
+            x (jnp.ndarray): states, (state_size, )
+            t (jnp.ndarray): time step, shape (1, )
+
+        Returns:
+            jnp:ndarray: terminal cost, shape (1, )
+        """
+        return (
+            6.0 * (x[0] ** 2)
+            + 12.0 * ((jnp.cos(x[2] - np.pi) + 1.0) ** 2)
+            + 0.1 * (x[1] ** 2)
+            + 0.1 * (x[3] ** 2)
+        ) * self._terminal_weight
