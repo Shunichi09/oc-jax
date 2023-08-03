b17433a74dd77bc7c3b86f107ef7e0ff06fd6a29
Shunichi09 <quick1st97@gmail.com>
2023-08-04 01:38:56 +0900

    Add new key func

--- apop/random.py
+++ apop/random.py
@@ -1,8 +1,15 @@
 import numpy as np
 
+import jax
+
 drng = np.random.default_rng()
 
 
 def seed(seed: int):
     global drng
     drng = np.random.default_rng(seed)
+
+
+def new_key(key: jax.random.KeyArray):
+    _, subkey = jax.random.split(key)
+    return subkey
