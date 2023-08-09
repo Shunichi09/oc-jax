444f9f7740013ef3739b4ea69209a63ea58e25e2
Shunichi09 <quick1st97@gmail.com>
2023-08-10 01:28:35 +0900

    Add mountaincar example

---
+++ apop/transition_models/classic_control/mountain_car.py
@@ -0,0 +1,64 @@
+from functools import partial
+
+import jax
+from jax import numpy as jnp
+
+from apop.transition_model import TransitionModel
+
+
+class MountainCarModel(TransitionModel):
+    def __init__(
+        self,
+        u_max: float = 1.0,
+        u_min: float = -1.0,
+        max_speed: float = 0.07,
+        min_speed: float = -0.07,
+        min_position: float = -1.2,
+        max_position: float = 0.6,
+        power: float = 0.0015,
+    ):
+        super().__init__()
+        self._u_max = u_max
+        self._u_min = u_min
+        self._max_speed = max_speed
+        self._min_speed = min_speed
+        self._max_position = max_position
+        self._min_position = min_position
+        self._power = power
+
+    @partial(jax.jit, static_argnums=(0,))
+    def predict_next_state(
+        self, x: jnp.ndarray, u: jnp.ndarray, t: jnp.ndarray
+    ) -> jnp.ndarray:
+        """predict next state
+
+        Args:
+            x (jnp.ndarray): states, shape (state_size, )
+            u (jnp.ndarray): input, shape (input_size, )
+            t (jnp.ndarray): time step, shape (1, )
+
+        Returns:
+            next_x (jnp.ndarray): next state, shape (state_size, )
+        """
+        position = x[0]
+        velocity = x[1]
+        force = jnp.clip(u[0], a_max=self._u_max, a_min=self._u_min)
+
+        velocity = velocity + force * self._power - 0.0025 * jnp.cos(3 * position)
+        velocity = jnp.clip(velocity, a_min=self._min_speed, a_max=self._max_speed)
+        position = position + velocity
+        position = jnp.clip(
+            position, a_min=self._min_position, a_max=self._max_position
+        )
+
+        velocity = (
+            jnp.float32(
+                jnp.logical_and(position > self._min_position, velocity > 0),
+            )
+            * velocity
+        )
+
+        next_x = jnp.empty_like(x)
+        next_x = next_x.at[0].set(position)
+        next_x = next_x.at[1].set(velocity)
+        return next_x
---
+++ examples/mppi/mountain_car.py
@@ -0,0 +1,95 @@
+import argparse
+import os
+
+import gymnasium
+import numpy as np
+from gymnasium.wrappers.record_video import RecordVideo
+
+# pip install gymnasium[classic-control]
+from jax import numpy as jnp
+
+import apop
+from apop.controllers.mppi import MPPI
+from apop.cost_function import QuadraticCostFunction
+from apop.transition_models.classic_control.mountain_car import MountainCarModel
+
+
+def run(args):
+    # build env
+    if args.record_video:
+        env = RecordVideo(
+            gymnasium.make(
+                "MountainCarContinuous-v0", render_mode="rgb_array", goal_velocity=0.0
+            ),
+            video_folder=args.video_path,
+        )
+    else:
+        env = gymnasium.make(
+            "MountainCarContinuous-v0", render_mode="human", goal_velocity=0.0
+        )
+
+    # build controller
+    cost_function = QuadraticCostFunction(
+        Q=jnp.diag(jnp.array([0.0, 0.0])),
+        Qf=jnp.diag(jnp.array([1000.0, 10.0])),
+        R=jnp.eye(1) * 0.01,  # NOTE: In mppi, R is ignored.
+        F=jnp.zeros((2, 1)),
+        target_state=jnp.array([0.5, 0.0]),  # NOTE: reach larger than 0.45 so set 0.5.
+    )
+    transition_model = MountainCarModel()
+    T = 50
+    controller = MPPI(
+        transition_model,
+        cost_function,
+        T,
+        sample_size=500,
+        alpha=1.0,
+        gamma=1.0,
+        lmb=1.0,
+        initial_covariance=np.eye(1) * 1.5,
+        upper_bound=np.array([1.0]),
+        lower_bound=np.array([-1.0]),
+    )
+
+    # run control
+    state, info = env.reset()
+    terminated = False
+    truncated = False
+
+    optimized_u_sequence = jnp.zeros((T, 1))
+    total_score = 0.0
+    while not (terminated or truncated):
+        if args.random_action:
+            action = env.action_space.sample()
+        else:
+            optimized_u_sequence = controller.control(
+                jnp.array(state), optimized_u_sequence
+            )
+
+        action = np.array(optimized_u_sequence[0])
+        next_state, reward, terminated, truncated, info = env.step(action)
+        state = next_state
+        env.render()
+        total_score += reward
+        # forward inputs
+        optimized_u_sequence = jnp.concatenate(
+            [optimized_u_sequence[1:], jnp.zeros((1, 1))], axis=0
+        )
+        print(f"state = {state}")
+        print(f"total_score = {total_score}")
+
+    env.close()
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--record_video", action="store_true")
+    parser.add_argument("--random_action", action="store_true")
+    default_path = os.path.join(os.path.dirname(__file__), "video")
+    parser.add_argument("--video_path", type=str, default=default_path)
+    args = parser.parse_args()
+    run(args)
+
+
+if __name__ == "__main__":
+    main()
