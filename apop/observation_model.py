247cc0ef4e69383b92c9a88ab10e70e9cdbf4d1c
Shunichi09 <quick1st97@gmail.com>
2023-08-16 01:18:28 +0900

    Change api to handle key and remove prob and dete model

--- apop/observation_model.py
+++ apop/observation_model.py
@@ -1,6 +1,5 @@
 from abc import ABCMeta
 from functools import partial
-from typing import Optional
 
 import jax
 from jax import numpy as jnp
@@ -14,62 +13,54 @@ class ObservationModel(metaclass=ABCMeta):
 
     @partial(jax.jit, static_argnums=(0,))
     def observe_batched(
-        self, curr_x: jnp.ndarray, observation_mask: jnp.ndarray
+        self,
+        curr_x: jnp.ndarray,
+        observation_mask: jnp.ndarray,
+        random_key: jax.random.KeyArray,
     ) -> jnp.ndarray:
         """observe y from x
 
         Args:
             curr_x (jnp.ndarray): current state, shape (batch_size, state_size)
             observation_mask (jnp.ndarray): observation mask, shape (batch_size, )
+            random_key (jax.random.KeyArray): random key
 
         Returns:
             jnp.ndarray: observation state, shape (batch_size, observation_size)
         """
-        batched_obs_func = jax.vmap(self.observe, in_axes=(0, None), out_axes=0)
-        return batched_obs_func(curr_x, observation_mask)
+        batched_obs_func = jax.vmap(self.observe, in_axes=(0, None, 0), out_axes=0)
+        return batched_obs_func(
+            curr_x, observation_mask, jax.random.split(random_key, curr_x.shape[0])
+        )
 
     @partial(jax.jit, static_argnums=(0,))
     def observe(
-        self, curr_x: jnp.ndarray, observation_mask: jnp.ndarray
+        self,
+        curr_x: jnp.ndarray,
+        observation_mask: jnp.ndarray,
+        random_key: jax.random.KeyArray,
     ) -> jnp.ndarray:
         """observe y from x
 
         Args:
             curr_x (jnp.ndarray): current state, shape (state_size, )
             observation_mask (jnp.ndarray): observation mask
+            random_key (jax.random.KeyArray): random key
 
         Returns:
             jnp.ndarray: observation state, shape (observation_size, )
         """
         raise NotImplementedError
 
-
-class DeterministicObservationModel(ObservationModel, metaclass=ABCMeta):
-    def __init__(self):
-        super().__init__()
-
-    def is_deterministic(self):
-        return True
-
-
-class ProbabilisticObservationModel(ObservationModel, metaclass=ABCMeta):
-    _key: jax.random.KeyArray
-
-    def __init__(self, key: jax.random.KeyArray):
-        super().__init__()
-        self._key = key
-
-    def is_probabilistic(self):
-        return True
-
     def observe_distribution(
-        self, curr_x: jnp.ndarray, observation_mask: Optional[jnp.ndarray] = None
+        self, curr_x: jnp.ndarray, observation_mask: jnp.ndarray
     ) -> Distribution:
         """observe y from x
 
         Args:
             curr_x (jnp.ndarray): current state, shape (state_size, )
             observation_mask (jnp.ndarray): observation mask
+
         Returns:
             Distribution: observation state distribution
         """
