71fab79f96939b05e8545564af010a7a4df3b6bc
Shunichi09 <quick1st97@gmail.com>
2023-08-16 01:25:24 +0900

    Update to support new api  and add tests for sampling different value

--- tests/observation_models/basic/test_linear.py
+++ tests/observation_models/basic/test_linear.py
@@ -7,6 +7,7 @@ from apop.observation_models.basic.linear import (
     LinearGaussianObservationModel,
 )
 from apop.random import np_drng
+import jax
 
 
 class TestLinearObservationModel:
@@ -16,7 +17,11 @@ class TestLinearObservationModel:
 
         curr_x = np_drng.normal(size=(5, 4))
         mask = jnp.ones(5, dtype=jnp.bool_)
-        actual = np.array(obs_model.observe_batched(curr_x, mask).block_until_ready())
+        actual = np.array(
+            obs_model.observe_batched(
+                curr_x, mask, jax.random.PRNGKey(10)
+            ).block_until_ready()
+        )
 
         for i in range(curr_x.shape[0]):
             expected = np.matmul(C, curr_x[i][:, np.newaxis]).flatten()
@@ -30,5 +35,27 @@ class TestLinearGaussianObservationModel:
         obs_model = LinearGaussianObservationModel(jnp.array(C), jnp.array(cov))
         curr_x = np_drng.normal(size=(5, 4))
         mask = jnp.ones(5, dtype=jnp.bool_)
-        actual = np.array(obs_model.observe_batched(curr_x, mask).block_until_ready())
+        actual = np.array(
+            obs_model.observe_batched(
+                curr_x, mask, jax.random.PRNGKey(10)
+            ).block_until_ready()
+        )
         assert actual.shape == (5, 3)
+
+    def test_observe_batched_sample_different_value(self):
+        C = np_drng.normal(size=(3, 4))
+        cov = np.eye(3)
+        obs_model = LinearGaussianObservationModel(jnp.array(C), jnp.array(cov))
+        curr_x = np_drng.normal(size=(5, 4))
+        mask = jnp.ones(5, dtype=jnp.bool_)
+        actual1 = np.array(
+            obs_model.observe_batched(
+                curr_x, mask, jax.random.PRNGKey(10)
+            ).block_until_ready()
+        )
+        actual2 = np.array(
+            obs_model.observe_batched(
+                curr_x, mask, jax.random.PRNGKey(100)
+            ).block_until_ready()
+        )
+        assert not np.allclose(actual1, actual2)
