031e03b0d1804dfeacfb311db1762b4ac6c6c868
Shunichi09 <quick1st97@gmail.com>
2023-08-16 01:22:15 +0900

    Update ball tracking model to sample from not gaussian

--- apop/transition_models/mujoco/ball_tracking.py
+++ apop/transition_models/mujoco/ball_tracking.py
@@ -3,25 +3,50 @@ from functools import partial
 import jax
 from jax import numpy as jnp
 
-from apop.transition_model import ProbabilisticTransitionModel
+from apop.distribution import Distribution
 from apop.distributions.gaussian import Gaussian
-from apop.random import new_key
+from apop.transition_model import TransitionModel
 
 
-class Ball2dTrackingGaussianTransitionModel(ProbabilisticTransitionModel):
+class SimpleTwoWheeleDistribution(Distribution):
     def __init__(
         self,
+        curr_x: jnp.ndarray,
+        curr_u: jnp.ndarray,
+        input_covariance: jnp.ndarray,
         dt: float,
-        covariance: jnp.ndarray,
-        key: jax.random.KeyArray = jax.random.PRNGKey(0),
-    ):
-        super().__init__(key)
+    ) -> None:
+        super().__init__()
+        self._curr_x = curr_x
+        self._curr_u = Gaussian(curr_u, input_covariance)
         self._dt = dt
-        self._covariance = covariance
+
+    @partial(jax.jit, static_argnums=(0, 2))
+    def sample(self, random_key: jax.random.KeyArray, num_samples: int) -> jnp.ndarray:
+        curr_u = self._curr_u.sample(random_key, num_samples)
+        pos_x = curr_u[:, 0] * self._dt * jnp.cos(self._curr_x[2]) + self._curr_x[0]
+        pos_y = curr_u[:, 0] * self._dt * jnp.sin(self._curr_x[2]) + self._curr_x[1]
+        pos_theta = curr_u[:, 1] * self._dt + self._curr_x[2]
+        sampled = jnp.concatenate(
+            [pos_x[:, jnp.newaxis], pos_y[:, jnp.newaxis], pos_theta[:, jnp.newaxis]],
+            axis=1,
+        )
+        return sampled
+
+
+class Ball2dTrackingGaussianTransitionModel(TransitionModel):
+    def __init__(self, dt: float, input_covariance: jnp.ndarray):
+        super().__init__()
+        self._dt = dt
+        self._input_covariance = input_covariance
 
     @partial(jax.jit, static_argnums=(0,))
     def predict_next_state(
-        self, x: jnp.ndarray, u: jnp.ndarray, t: jnp.ndarray
+        self,
+        x: jnp.ndarray,
+        u: jnp.ndarray,
+        t: jnp.ndarray,
+        random_key: jax.random.KeyArray,
     ) -> jnp.ndarray:
         """predict next state
 
@@ -29,16 +54,17 @@ class Ball2dTrackingGaussianTransitionModel(ProbabilisticTransitionModel):
             x (jnp.ndarray): states, shape (state_size, )
             u (jnp.ndarray): input, shape (input_size, )
             t (jnp.ndarray): time step, shape (1, )
+            random_key (jax.random.KeyArray): random key
 
         Returns:
             next_x (jnp.ndarray): next state, shape (state_size, )
         """
         distribution = self.predict_next_state_distribution(x, u, t)
-        return distribution.sample(1)[0]
+        return distribution.sample(random_key, 1)[0]
 
     def predict_next_state_distribution(
         self, x: jnp.ndarray, u: jnp.ndarray, t: jnp.ndarray
-    ) -> Gaussian:
+    ) -> SimpleTwoWheeleDistribution:
         """predict next state
 
         Args:
@@ -49,9 +75,4 @@ class Ball2dTrackingGaussianTransitionModel(ProbabilisticTransitionModel):
         Returns:
             Distribution: next state distribution
         """
-        pos_x = u[0] * self._dt * jnp.cos(x[2]) + x[0]
-        pos_y = u[0] * self._dt * jnp.sin(x[2]) + x[1]
-        pos_theta = u[1] * self._dt + x[2]
-        mean = jnp.array([pos_x, pos_y, pos_theta])
-        self._key = new_key(self._key)
-        return Gaussian(self._key, mean, self._covariance)
+        return SimpleTwoWheeleDistribution(x, u, self._input_covariance, self._dt)
